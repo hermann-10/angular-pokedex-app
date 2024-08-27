f72aa225e01a10d62afedf5444681ad5b71c5ac8
HM-Dev <hermann.manuel@hotmail.com>
2024-08-27 20:50:36 +0200

    Configuration du routage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+
+
+const routes: Routes = [
+  { path: 'pokemons', component: PokemonListComponent },
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ],
 };
