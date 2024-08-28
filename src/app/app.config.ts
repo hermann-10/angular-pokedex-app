f07994031dd98d1d328281fb79365728782f9b6c
HM-Dev <hermann.manuel@hotmail.com>
2024-08-28 11:09:05 +0200

    Création d'une page not found pour les liens invalides + ajout des titres pour chaque page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  { path: 'pokemons/:id', component: PokemonProfileComponent},
-  { path: 'pokemons', component: PokemonListComponent },
+  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokemon'},
+  { path: 'pokemons', component: PokemonListComponent, title: 'Pokedex' },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent, title: 'Page Introuvable' },
 ];
 
 export const appConfig: ApplicationConfig = {
